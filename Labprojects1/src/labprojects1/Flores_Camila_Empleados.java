efbb61ddc95dfb5ddeefef2b63393a775728a822
camilaflores07 <camila_maria@unitec.edu>
2024-10-18 08:44:33 -0600

    Creacion de variables

---
+++ Labprojects1/src/labprojects1/Flores_Camila_Empleados.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package labprojects1;
+
+/**
+ *
+ * @author 50494
+ */
+public class Flores_Camila_Empleados {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+       String NombreEmpleado;
+       int HorasLaborales;
+       double SalariopHora;
+       
+             
+       
+        
+    }
+    
+}
